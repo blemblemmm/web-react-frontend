109babef97cecc166187936398e9396d9c3d8a60
blemblemmm <bachlam138@gmail.com>
2023-01-01 07:32:23 +0700

    profile

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -42,7 +42,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
         <UserImage image={userPicturePath} size="55px" />
         <Box
           onClick={() => {
-            navigate(`/profile/${friendId}`);
+            navigate(`https://sociopedia-web-api.onrender.com/profile/${friendId}`);
             navigate(0);
           }}
         >
